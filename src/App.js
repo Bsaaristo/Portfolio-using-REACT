973cb699b2fdaaa7c71569b60ddbd450488d2a78
Bsaaristo <briansaaristojr@icloud.com>
2023-01-20 23:33:06 -0600

    useState updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import MainDivs from "./components/MainDivs";
+import { useState } from "react";
 
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function decrementCount() {
+    setCount(count - 1);
+  }
+  function incrementCount() {
+    setCount(count + 1);
+  }
   return (
     <div>  <Header />
     <MainDivs />
     <Home />
-    <additionBox /></div>
+    <additionBox />
+    <button onClick={decrementCount}>-</button>
+    <span>{count}</span>
+    <button onClick={incrementCount}>+</button></div>
   );
 }
 
